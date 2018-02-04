ca62449877a6a10236301e64ea4f5249b08aa276
Cupcake Zeera <yragrande01@gmail.com>
2018-02-04 23:56:11 +0800

    add login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,3 +7,32 @@ import { AuthService } from './core/auth.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
+
+constructor(public afService: AF, private router: Router) {
+    // This asynchronously checks if our user is logged it and will automatically
+    // redirect them to the Login page when the status changes.
+    // This is just a small thing that Firebase does that makes it easy to use.
+    this.afService.af.auth.subscribe(
+      (auth) => {
+        if(auth == null) {
+          console.log("Not Logged in.");
+          this.isLoggedIn = false;
+          this.router.navigate(['login']);
+        }
+        else {
+          console.log("Successfully Logged in.");
+          // Set the Display Name and Email so we can attribute messages to them
+          if(auth.google) {
+            this.afService.displayName = auth.google.displayName;
+            this.afService.email = auth.google.email;
+          }
+          else {
+            this.afService.displayName = auth.auth.email;
+            this.afService.email = auth.auth.email;
+          }
+          this.isLoggedIn = true;
+          this.router.navigate(['']);
+        }
+      }
+    );
+  }
